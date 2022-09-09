6f23515498dd830479671a8a63361657f4754b32
Demir Boraj <demirboraj97@gmail.com>
2022-09-09 02:52:44 +0200

    1

--- db/setup.ts
+++ db/setup.ts
@@ -86,3 +86,75 @@ INSERT INTO interviewers (name, email) VALUES (@name, @email)
 for (let interviewer of interviewers) {
   createInterviewer.run(interviewer);
 }
+
+const interviews = [
+  {
+    applicantId: 1,
+    interviewerId: 1,
+    date: "2022-01-09",
+    result: "9/10",
+  },
+  {
+    applicantId: 3,
+    interviewerId: 1,
+    date: "2022-04-07",
+    result: "2/10",
+  },
+  {
+    applicantId: 1,
+    interviewerId: 2,
+    date: "2022-09-01",
+    result: "10/10",
+  },
+  {
+    applicantId: 2,
+    interviewerId: 4,
+    date: "2022-07-12",
+    result: "5/10",
+  },
+  {
+    applicantId: 4,
+    interviewerId: 2,
+    date: "2022-06-01",
+    result: "8/10",
+  },
+  {
+    applicantId: 4,
+    interviewerId: 3,
+    date: "2022-09-08",
+    result: "3/10",
+  },
+  {
+    applicantId: 3,
+    interviewerId: 4,
+    date: "2022-09-08",
+    result: "7/10",
+  },
+];
+
+const dropInterviewsTable = db.prepare(`
+DROP TABLE IF EXISTS interviews;
+`);
+dropInterviewsTable.run();
+
+const createInterviewsTable = db.prepare(`
+CREATE TABLE IF NOT EXISTS interviews (
+    id INTEGER NOT NULL,
+    applicantId INTEGER NOT NULL,
+    interviewerId INTEGER  NOT NULL,
+    date TEXT,
+    result TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (applicantId) REFERENCES applicants(id) ON DELETE CASCADE,
+    FOREIGN KEY (interviewerId) REFERENCES interviewers(id) ON DELETE CASCADE
+);
+`);
+createInterviewsTable.run();
+
+const createNewInterview = db.prepare(`
+INSERT INTO interviews (applicantId, interviewerId, date, result) VALUES (@applicantId, @interviewerId, @date, @result);
+`);
+
+for (let interview of interviews) {
+  createNewInterview.run(interview);
+}
